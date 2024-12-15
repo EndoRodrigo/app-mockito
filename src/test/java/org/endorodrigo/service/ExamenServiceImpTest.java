5e2ad3d3029d9155b7427fc88990cef8c9c0a74c
EndoRodrigoRodriguez <endorodrigo19@gmail.com>
2024-12-15 18:08:26 -0500

    feat: creacion del primer test con mockito

--- src/test/java/org/endorodrigo/service/ExamenServiceImpTest.java
+++ src/test/java/org/endorodrigo/service/ExamenServiceImpTest.java
@@ -2,17 +2,24 @@ package org.endorodrigo.service;
 
 import org.endorodrigo.model.Examen;
 import org.endorodrigo.repository.ExamenRepository;
-import org.endorodrigo.repository.ExamenRepositoryImp;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class ExamenServiceImpTest {
 
     @Test
     void findElemntForName() {
-        ExamenRepository repository = new ExamenRepositoryImp();
+        List<Examen> datos = Arrays.asList(new Examen(5L, "MATEMATICAS"), new Examen(6L, "LENGUAJE"), new Examen(7L, "HISTORIA"));
+
+        ExamenRepository repository = mock(ExamenRepository.class);
         ExamenService service = new ExamenServiceImp(repository);
+
+        when(repository.findAll()).thenReturn(datos);
         Examen examen = service.findElemntForName("MATEMATICAS");
         assertNotNull(examen);
         assertEquals(5L,examen.getId());
